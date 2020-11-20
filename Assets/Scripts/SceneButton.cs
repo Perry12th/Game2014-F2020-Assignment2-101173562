9c47da7304a1276df59fffb16af276914bfae4ef
FlavioJorge AraujoSilva <silvaflavio267@gmail.com>
2020-11-20 12:05:11 -0500

    Assignment 2 Part 1 Commit

---
+++ Assets/Scripts/SceneButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButton : MonoBehaviour
+{
+    [SerializeField]
+
+    private string sceneName;
+    public void OnClicked()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
